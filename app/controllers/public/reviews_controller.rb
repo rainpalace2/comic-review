04f2c3c5c0e4559ad93a323a8e126ddbe7cdaa1a
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2022-10-05 07:06:12 +0000

    [Fix]星レビュー機能

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,4 +1,6 @@
 class Public::ReviewsController < ApplicationController
+  before_action :authenticate_customer!, only: [:create]
+  
   def index
     @book = Book.find(params[:book_id])
     @reviews = @book.reviews
@@ -8,7 +10,7 @@ class Public::ReviewsController < ApplicationController
     @review = Review.new(review_prams)
     # @review.customer_id = current_customer.id
     if @review.save
-      redirect_to book_review_path(@review.book)
+      redirect_to book_reviews_path(@review.book)
     else
        @book = Book.find(params[:book_id])
       render "public/books/show"
@@ -18,7 +20,7 @@ class Public::ReviewsController < ApplicationController
   private
 
   def review_prams
-    params.require(:review).permit(:book_id, :score, :content, :customer_id)
+    params.require(:review).permit(:book_id, :score, :score, :content, :customer_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  
-  
 
-  
+
+
+
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin,skip: [:registrations,:passwords], controllers: {
